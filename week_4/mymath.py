31114180c8e6c2ab4d2507976ca194c61822d25d
Bekarys <bekaryserezep05@gmail.com>
2025-11-16 19:22:31 +0500

    4-апта тапсырмасының жауабын жүктедім

---
+++ week_4/mymath.py
@@ -0,0 +1,15 @@
+
+
+def add(a: int, b: int) -> int:
+    return a + b
+
+def sub(a: int, b: int) -> int:
+    return a - b
+
+def mul(a: int, b: int) -> int:
+    return a * b
+
+def div(a: int, b: int) -> float:
+    if b == 0:
+        raise ValueError("Cannot divide by zero.")
+    return a / b
